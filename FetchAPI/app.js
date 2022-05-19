a9bd40c29f48b22fa2e5cbd4475dedcfa3357709
Akshay <96732108+thunder-volt@users.noreply.github.com>
2022-05-19 13:41:30 +0530

    updated

--- FetchAPI/app.js
+++ FetchAPI/app.js
@@ -1,171 +1,21 @@
 
-        getData();
-        const root = document.createElement('div');
-                    const info1 = document.createElement('div');
-                    const info2 = document.createElement('div');
-                    const info3 = document.createElement('div');
-                    const info4 = document.createElement('div');
-                    const info5 = document.createElement('div');
-                    const info6 = document.createElement('div');
-                    const info7 = document.createElement('div');
-                    const info8 = document.createElement('div');
-                    const info9 = document.createElement('div');
-                    const info10 = document.createElement('div');
-                    const info11 = document.createElement('div');
-                    const info12 = document.createElement('div');
-                    const info13 = document.createElement('div');
-                    const info14 = document.createElement('div');
-                    const info15 = document.createElement('div');
-                    const info16 = document.createElement('div');
-                    const info17 = document.createElement('div');
-                    const info18 = document.createElement('div');
-                    const info19 = document.createElement('div');
-                    const info20 = document.createElement('div');
-                    const info21 = document.createElement('div');
-                    const info22 = document.createElement('div');
-                    const info23 = document.createElement('div');
-                    const info24 = document.createElement('div');
-                    const info25 = document.createElement('div');
-                    const info26 = document.createElement('div');
-                    const info27 = document.createElement('div');
-                    const info28 = document.createElement('div');
-                    const info29 = document.createElement('div');
-                    const info30 = document.createElement('div');
-                    const info31 = document.createElement('div');
-                    const info32 = document.createElement('div');
-                    const info33 = document.createElement('div');
-                    const info34 = document.createElement('div');
-                    const info35 = document.createElement('div');
-                    const info36 = document.createElement('div');
-                    const info37 = document.createElement('div');
-                    const info38 = document.createElement('div');
-                    const info39 = document.createElement('div');
-                    const info40 = document.createElement('div');
-                    const info41 = document.createElement('div');
-                    const info42 = document.createElement('div');
-                    const info43 = document.createElement('div');
-                    const info44 = document.createElement('div');
-                    const info45 = document.createElement('div');
-                    const info46 = document.createElement('div');
-                    const info47 = document.createElement('div');
-                    const info48 = document.createElement('div');
-                    const info49 = document.createElement('div');
-                    const info50 = document.createElement('div');
-        async function getData() {
-            fetch('https://retoolapi.dev/PNAOH1/data')
-                .then(response => response.json())
-                .then(data => {
-                    JSON.stringify(data);
-       
-                    root.append(info1
-                        , info2
-                        , info3
-                        , info4
-                        , info5
-                        , info6
-                        , info7
-                        , info8
-                        , info9
-                        , info10
-                        , info11
-                        , info12
-                        , info13
-                        , info14
-                        , info15
-                        , info16
-                        , info17
-                        , info18
-                        , info19
-                        , info20
-                        , info21
-                        , info22
-                        , info23
-                        , info24
-                        , info25
-                        , info26
-                        , info27
-                        , info28
-                        , info29
-                        , info30
-                        , info31
-                        , info32
-                        , info33
-                        , info34
-                        , info35
-                        , info36
-                        , info37
-                        , info38
-                        , info39
-                        , info40
-                        , info41
-                        , info42
-                        , info43
-                        , info44
-                        , info45
-                        , info46
-                        , info47
-                        , info48
-                        , info49
-                        , info50
+getdata();
+var root = document.createElement('div');
 
-                    );
-
-                    info1.textContent = `${Object.values(data[0])}`;
-                    info2.textContent = `${Object.values(data[1])}`;
-                    info3.textContent = `${Object.values(data[2])}`;
-                    info4.textContent = `${Object.values(data[3])}`;
-                    info5.textContent = `${Object.values(data[4])}`;
-                    info6.textContent = `${Object.values(data[5])}`;
-                    info7.textContent = `${Object.values(data[6])}`;
-                    info8.textContent = `${Object.values(data[7])}`;
-                    info9.textContent = `${Object.values(data[8])}`;
-                    info10.textContent = `${Object.values(data[9])}`;
-                    info11.textContent = `${Object.values(data[10])}`;
-                    info12.textContent = `${Object.values(data[11])}`;
-                    info13.textContent = `${Object.values(data[12])}`;
-                    info14.textContent = `${Object.values(data[13])}`;
-                    info15.textContent = `${Object.values(data[14])}`;
-                    info16.textContent = `${Object.values(data[15])}`;
-                    info17.textContent = `${Object.values(data[16])}`;
-                    info18.textContent = `${Object.values(data[17])}`;
-                    info19.textContent = `${Object.values(data[18])}`;
-                    info20.textContent = `${Object.values(data[19])}`;
-                    info21.textContent = `${Object.values(data[20])}`;
-                    info22.textContent = `${Object.values(data[21])}`;
-                    info23.textContent = `${Object.values(data[22])}`;
-                    info24.textContent = `${Object.values(data[23])}`;
-                    info25.textContent = `${Object.values(data[24])}`;
-                    info26.textContent = `${Object.values(data[25])}`;
-                    info27.textContent = `${Object.values(data[26])}`;
-                    info28.textContent = `${Object.values(data[27])}`;
-                    info29.textContent = `${Object.values(data[28])}`;
-                    info30.textContent = `${Object.values(data[29])}`;
-                    info31.textContent = `${Object.values(data[30])}`;
-                    info32.textContent = `${Object.values(data[31])}`;
-                    info33.textContent = `${Object.values(data[32])}`;
-                    info34.textContent = `${Object.values(data[33])}`;
-                    info35.textContent = `${Object.values(data[34])}`;
-                    info36.textContent = `${Object.values(data[35])}`;
-                    info37.textContent = `${Object.values(data[36])}`;
-                    info38.textContent = `${Object.values(data[37])}`;
-                    info39.textContent = `${Object.values(data[38])}`;
-                    info40.textContent = `${Object.values(data[39])}`;
-                    info41.textContent = `${Object.values(data[40])}`;
-                    info42.textContent = `${Object.values(data[41])}`;
-                    info43.textContent = `${Object.values(data[42])}`;
-                    info44.textContent = `${Object.values(data[43])}`;
-                    info45.textContent = `${Object.values(data[44])}`;
-                    info46.textContent = `${Object.values(data[45])}`;
-                    info47.textContent = `${Object.values(data[46])}`;
-                    info48.textContent = `${Object.values(data[47])}`;
-                    info49.textContent = `${Object.values(data[48])}`;
-                    info50.textContent = `${Object.values(data[49])}`;
-    
-                    document.body.append(root);
-                    console.log(data)
-                })
-                .catch(error => console.error(error))
-            console.log(data);
-          
+function getdata() {
+    fetch('https://retoolapi.dev/PNAOH1/data')
+        .then(response => response.json())
+        .then(data => {
+            JSON.stringify(data);
+            document.body.append(root);
+            for (let i = 0; i < 51; i++) {
+                var div = document.createElement("div");
+                div.innerHTML = `${Object.values(data[i])}`;
+                root.appendChild(div);
+            }
+            console.log(data)
         }
-        root.className = "body";
+        )
+}
+
+root.className = "body";
